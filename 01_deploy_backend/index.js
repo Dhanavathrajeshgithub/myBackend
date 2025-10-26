72c0bfa1e48e5250babb7855498a1d272f70930a
Dhanavath Rajesh <dhanavathrajeshgithub@gmail.com>
2025-10-26 21:39:51 +0530

    First Project deploying

---
+++ 01_deploy_backend/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/login", (req, res) => {
+  res.send("user loggein");
+});
+app.get("/signup", (req, res) => {
+  res.send("<h1>You are signed in</h1>");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
