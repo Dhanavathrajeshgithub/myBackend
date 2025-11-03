6adc720bbc43ec2025ac1bebec3c54891579f55a
Dhanavath Rajesh <dhanavathrajeshgithub@gmail.com>
2025-11-03 17:08:10 +0530

    added two models users and video. Inside video jwt and bcypt methods are written

---
+++ 04_Mega_Project/src/models/video.model.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = mongoose.model("Video", videoSchema);
