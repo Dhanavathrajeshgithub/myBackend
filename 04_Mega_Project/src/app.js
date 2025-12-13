5898508578ae4ba087c8c9d43d227a720cab2e81
Dhanavath Rajesh <dhanavathrajeshgithub@gmail.com>
2025-12-13 19:28:17 +0530

    written basic user controller

--- 04_Mega_Project/src/app.js
+++ 04_Mega_Project/src/app.js
@@ -14,3 +14,9 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+import userRoute from "./routes/user.route.js";
+
+// routes declaration
+app.use("/api/v1/users", userRoute);
--- 04_Mega_Project/src/utils/asyncHandler.js
+++ 04_Mega_Project/src/utils/asyncHandler.js
@@ -14,4 +14,4 @@ const asyncHandler = (fn) => {
   };
 };
 
-export { asyncHandler };
+export default asyncHandler;
